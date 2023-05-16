77771df30c646165c7a44f6f29b8b263969b20b5
adisornkosajan <mol.adisornkosajan@gmail.com>
2023-05-16 10:05:25 +0700

    my first commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+ <nav >
+    <h1>Kmshop</h1>
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="/about">เกี่่ยวกับเรา</NuxtLink>
+    </nav>
+</header>
+</template>
+
+<script setup>
+
+</script>
+
+<style  scoped>
+nav{
+    margin: 10px auto;
+    padding: 10px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #c5d4e4;
+}
+</style>
